55ff0e5356a1a8361b0e8a0e16391541abf0ba70
Kobi <Kobi@Lenovo-PC>
2015-10-10 10:17:49 +0300

    Changed generator

--- MVP game/src/Tests/AstarTests.java
+++ MVP game/src/Tests/AstarTests.java
@@ -27,30 +27,37 @@ public class AstarTests{
 	SearchableMaze3d randomSearchableMazeToTest;
 	AStarCommonSearcher aStar;
 	
+	SearchableMaze3d noneSearchableMaze;
+	SearchableMaze3d oneCellWallSearchableMaze;
+	SearchableMaze3d oneCellnotWallSearchableMaze; 
+	SearchableMaze3d startEqualsToGoalSearchableMaze;
+	
 	
 	@Before
 	public void setUp(){
 		MyMaze3dGenerator mg = new MyMaze3dGenerator();
-	
-
-		
-		Maze3d mazeToTest = mg.generate(3, 3, 3);
-
-		Maze3d randomMaze = mg.generate(10, 10,10);
-		searchableMazeToTest = new SearchableMaze3d(mazeToTest);
-		randomSearchableMazeToTest = new SearchableMaze3d(randomMaze);
-		
-		
-
-
-		
-		
 		aStar = new AStarCommonSearcher(new ManhatenDistance());
 		Maze3dSolution solution = new Maze3dSolution();
 		aStar.setSolution(solution);
-
 		
+		Maze3d mazeToTest = mg.generate(3, 3, 3);
+		Maze3d randomMaze = mg.generate(10, 10,10);
+		Maze3d noneMaze = new Maze3d(1, 1, 1);
+		Maze3d oneCellWallMaze = new Maze3d(1,1,1);
+		Maze3d oneCellnotWallMaze = new Maze3d(1,1,1);
+		Maze3d startEqualsToGoal = mg.generate(2, 2, 2);
+		startEqualsToGoal.setGoalPosition(startEqualsToGoal.getStartPosition());
+		int[][][] oneCellWallMazeArr = {{{1}}};
+		int[][][] oneCellNotWallMazeArr = {{{1}}};	
+		oneCellWallMaze.setMaze(oneCellWallMazeArr);
+		oneCellnotWallMaze.setMaze(oneCellNotWallMazeArr);
 		
+		noneSearchableMaze = new SearchableMaze3d(noneMaze);
+		oneCellWallSearchableMaze = new SearchableMaze3d(oneCellWallMaze );
+		oneCellnotWallSearchableMaze = new SearchableMaze3d(oneCellnotWallMaze);
+		startEqualsToGoalSearchableMaze = new SearchableMaze3d(startEqualsToGoal);
+		searchableMazeToTest = new SearchableMaze3d(mazeToTest);
+		randomSearchableMazeToTest = new SearchableMaze3d(randomMaze);
 	}
 	
 	@Test
@@ -59,10 +66,10 @@ public class AstarTests{
 		Assert.assertNotEquals(aStar.search(randomSearchableMazeToTest).getSolution().size(),0 );
 	}
 	
-/*	@Test
+	@Test
 	public void testMazeAstarSolutionIsNotNull() {
 		Assert.assertNotEquals(aStar.search(searchableMazeToTest).toString(),"" );
-	}*/
+	}
 	
 	@Test
 	public void testNegetiveAstarMazeSolutionAmountOfSteps() {
@@ -71,26 +78,14 @@ public class AstarTests{
 		Assert.assertNotEquals(aStar.getSolution().getSolution().size(),-1);
 	}
 	
-	
-	
-	
 	@Test
 	public void testMazeAstarEvaluatedNodesAmountOfSteps() {
 
 		Assert.assertNotEquals(aStar.getNumberOfNodesEvaluated(),-1);
 		Assert.assertNotEquals(aStar.getNumberOfNodesEvaluated(),6);
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	/*@Test
+		
+	@Test
 	public void testNoneMazeSolutionIsNotNull() {
 		Assert.assertNotEquals(aStar.search(noneSearchableMaze).toString(),null );
 	}
@@ -100,15 +95,10 @@ public class AstarTests{
 		Assert.assertEquals(aStar.search(noneSearchableMaze).toString(),"Solution:{}");
 	}
 	
-	
-	
-	
-	
 	@Test
 	public void oneCellWallMazeSolutionIsNotNull() {
 		Assert.assertNotEquals(aStar.search(oneCellWallSearchableMaze).toString(), null);
 	}
-	
 	
 	@Test
 	public void oneCellWallMazeSolutionIsEmpty() {
@@ -116,18 +106,14 @@ public class AstarTests{
 		Assert.assertEquals(aStar.search(oneCellWallSearchableMaze).toString(), "Solution:{}");
 	}
 	
-	
-	
-	
 	@Test
 	public void startEqualsToGoalMazeSolutionIsNotNull() {
 		Assert.assertNotEquals(aStar.search(startEqualsToGoalSearchableMaze).toString(), null);
 	}
 	
-	
 	@Test
 	public void startEqualsToGoalMazeSolutionIsEmpty() {
 		Assert.assertEquals(aStar.search(startEqualsToGoalSearchableMaze).toString(), "Solution:{}");
-	}*/
+	}
 	
 }
